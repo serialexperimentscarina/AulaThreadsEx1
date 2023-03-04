d41b789725eb5479b399c2432f38a6c1b1897a3a
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-03 21:54:30 -0300

    feat: Create principal

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,9 +1,16 @@
 package view;
 
+import controller.ThreadNumero;
+
 public class Principal {
 
 	public static void main(String[] args) {
-
+		
+		for (int i = 0; i < 5; i++) {
+			Thread threadNum = new ThreadNumero();
+			threadNum.start();
+		}
+		
 	}
 
 }
